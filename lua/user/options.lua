3938b03895f16c9eac3d04a5d4c67f897cba2da0
denisbordachkov <denisbordachkov@gmail.com>
2022-04-12 22:47:33 +0000

    Initial commit

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -26,7 +26,7 @@ local options = {
   tabstop = 2,                             -- insert 2 spaces for a tab
   cursorline = true,                       -- highlight the current line
   number = true,                           -- set numbered lines
-  relativenumber = false,                  -- set relative numbered lines
+  relativenumber = true,                  -- set relative numbered lines
   numberwidth = 4,                         -- set number column width to 2 {default 4}
   signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
   wrap = false,                            -- display lines as one long line
@@ -44,3 +44,18 @@ end
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+
+-- global 
+vim.cmd [[
+  let g:python3_host_prog = '/home/denis/.config/nvim.old/coc_python_env/venv/bin/python'
+]]
+
+-- Arrow fucker
+vim.cmd [[
+  nnoremap <Left> :echoe "Use h"<CR>
+  nnoremap <Right> :echoe "Use l"<CR>
+  nnoremap <Up> :echoe "Use k"<CR>
+  nnoremap <Down> :echoe "Use j"<CR>
+]]
+
+-- Cyrillic shortcuts enable
